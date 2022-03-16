acae81c5a54d7d011939ea127212b5d314793d94
Anca Balietti <anca.balietti@gmail.com>
2022-03-16 16:27:25 +0100

    end screen

--- game/client_types/player.js
+++ game/client_types/player.js
@@ -2339,7 +2339,7 @@ module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
                     mainText: 'Thank you for participating. ' +
                     '<br><br>' +
                     "If you want to get in touch with us for questions or suggestions, " +
-                    "please write us an email at <em><span style='color:#bf2b42'>pob.heidelberg@gmail.com</span></em>." +
+                    "please write us an email at <em><span style='color:#bf2b42'>academic.research.India@gmail.com</span></em>." +
                     '<br><br>' +
                     'We are very interested in ' +
                     'hearing your <strong>feedback</strong> about the ' +
@@ -2348,7 +2348,7 @@ module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
                     '<li>Did you find any question unclear or ' +
                     'uncomfortable?</li>' +
                     '<li>Did you experience any technical difficulty?</li>' +
-                    '<li>Was the map of the region of your choice loading correctly?</li>' +
+                    '<li>Were the images and maps loading correctly?</li>' +
                     '<li>How can we improve the study?</li></ol>'
                 }
             }
